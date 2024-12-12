4808797dfbf094f9cba91ead5df14837d223f783
hrishabhn <hrishabhn@outlook.com>
2024-12-12 14:25:12 +0100

    Remove template code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,5 @@
-import {ButtonExample} from './views/button'
-import {CalloutExample} from './views/callout'
-import {ColorsExample} from './views/colors'
-import {FormExample} from './views/form'
-import {HeadingExample} from './views/heading'
-import {MenuExample} from './views/menu'
-import {ThemesExample} from './views/themes'
-import {TooltipExample} from './views/tooltip'
-import {PageSection} from '@/components/layout'
+import {redirect} from 'next/navigation'
 
 export default function Home() {
-    return (
-        <PageSection>
-            <div className="space-y-4 py-4">
-                <HeadingExample />
-                <ColorsExample />
-                <ThemesExample />
-                <ButtonExample />
-                <MenuExample />
-                <FormExample />
-                <CalloutExample />
-                <TooltipExample />
-            </div>
-        </PageSection>
-    )
+    return redirect('/new')
 }
